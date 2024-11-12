7d6071fed5510cce12b78b60fe7f74546cf73554
SamsX2 <samux555iiiiiiii@gmail>
2024-11-12 12:40:37 -0500

    pasteleria

---
+++ src/controllers/pastelController.ts
@@ -0,0 +1,89 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../swagger/data-source";
+import { Pastel } from "../entities/Pastel";
+
+const PastelRepository = AppDataSource.getRepository(Pastel);
+
+// GET - Obtener Todos los Productos
+export const getAllPastel = async(red: Request, res: Response) => {
+  try {
+    const Pastel = await PastelRepository.find();
+    res.json(Pastel);
+  } catch(error) {
+    res.status(500).json({ message: "Error al obtener productos." });
+  }
+};
+
+// GET by ID - Obetener Producto por ID
+export const getPastelById = async(req: Request, res: Response) => {
+  try {
+    const Pastel = await PastelRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if(Pastel) {
+      res.json(Pastel);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al obtener el producto." });
+  }
+};
+
+// POST - Crear un nuevo Producto
+export const createPastel = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+    const pastel = new Pastel ();
+    pastel.name = name;
+    pastel.description = description;
+    pastel.price = price;
+
+    await PastelRepository.save(pastel);
+    res.status(201).json(Pastel);
+  } catch(error) {
+    res.status(500).json({ message: "Error al crear el producto." });
+  }
+};
+
+// PUT - Actualizar un Producto existente
+export const updatePastel = async(req: Request, res: Response) => {
+  try {
+    const { name, description, price } = req.body;
+    const Pastel = await PastelRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if(Pastel) {
+      Pastel.name = name ?? Pastel.name;
+      Pastel.description = description ?? Pastel.description;
+      Pastel.price = price ?? Pastel.price;
+
+      await PastelRepository.save(Pastel);
+      res.json(Pastel);
+    } else {
+      res.status(404).json({ message: "Producto no encontrado" });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al actualizar el producto." });
+  }
+};
+
+// DELETE - Borrar un Producto
+export const deletePastel = async(req: Request, res: Response) => {
+  try {
+    const Pastel = await PastelRepository.findOneBy({
+      id: parseInt(req.params.id),
+    });
+
+    if (Pastel) {
+      await PastelRepository.remove(Pastel);
+      res.json({ message: "Producto eliminado." });
+    } else {
+      res.status(404).json({ message: "Producto no encontrado." });
+    }
+  } catch(error) {
+    res.status(500).json({ message: "Error al eliminar el producto." });
+  }
+};
---
+++ src/routes/pastelRoutes.ts
@@ -0,0 +1,142 @@
+import { Router } from "express";
+import {
+  getAllPastel,
+  getPastelById,
+  createPastel,
+  updatePastel,
+  deletePastel,
+} from "../controllers/pastelController";
+
+const pastelRoutes = Router();
+
+/**
+ * @swagger
+ * tags:
+ *   name: Pastel
+ *   description: CRUD relacionado con productos
+ */
+
+/**
+ * @swagger
+ * /api/Pastel:
+ *   get:
+ *     summary: Obtener todos los productos
+ *     tags: [Products]
+ *     responses:
+ *       200:
+ *         description: Lista de productos
+ */
+pastelRoutes.get("/", getAllPastel);
+
+/**
+ * @swagger
+ * /api/Pastel/{id}:
+ *   get:
+ *     summary: Obtener un producto por ID
+ *     tags: [Products]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del producto
+ *     responses:
+ *       200:
+ *         description: Detalles del producto
+ *       404:
+ *         description: Producto no encontrado
+ */
+pastelRoutes.get("/:id", getPastelById);
+
+/**
+ * @swagger
+ * /api/Pastel:
+ *   post:
+ *     summary: Crear un nuevo producto
+ *     tags: [pastel]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - description
+ *               - price
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       201:
+ *         description: Producto creado
+ *       500:
+ *         description: Error en el servidor
+ */
+pastelRoutes.post("/", createPastel);
+
+/**
+ * @swagger
+ * /api/Pastel/{id}:
+ *   put:
+ *     summary: Actualizar un producto existente
+ *     tags: [Pastel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del producto
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               price:
+ *                 type: number
+ *     responses:
+ *       200:
+ *         description: Producto actualizado
+ *       404:
+ *         description: Producto no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+pastelRoutes.put("/:id", updatePastel);
+
+/**
+ * @swagger
+ * /api/Pastel/{id}:
+ *   delete:
+ *     summary: Eliminar un producto
+ *     tags: [Pastel]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID del producto
+ *     responses:
+ *       200:
+ *         description: Producto eliminado
+ *       404:
+ *         description: Producto no encontrado
+ *       500:
+ *         description: Error en el servidor
+ */
+pastelRoutes.delete("/:id", deletePastel);
+
+export default pastelRoutes;
